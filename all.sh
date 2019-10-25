f6fdd0b8d736b24927e32f90949caf49e6b4d510
aeleniumfor <huku8121@gmail.com>
2019-10-25 15:11:50 +0900

    sh all.sh

---
+++ all.sh
@@ -0,0 +1,62 @@
+yum -y update
+yum -y install git vim wget
+systemctl stop firewalld
+systemctl disable firewalld
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+# Install Docker CE
+## Set up the repository
+### Install required packages.
+yum install -y yum-utils device-mapper-persistent-data lvm2
+### Add Docker repository.
+yum-config-manager \
+  --add-repo \
+  https://download.docker.com/linux/centos/docker-ce.repo
+## Install Docker CE.
+yum -y update && yum -y install docker-ce-18.06.2.ce
+## Create /etc/docker directory.
+mkdir /etc/docker
+# Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+mkdir -p /etc/systemd/system/docker.service.d
+# Restart Docker
+systemctl daemon-reload && systemctl restart docker && systemctl enable docker
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+
+
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+systemctl enable --now kubelet
+cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+IPADDR=$(ip a show ens3 | grep inet | grep -v inet6 | awk '{print $2}' | cut -f1 -d/)
+sed -i "/KUBELET_EXTRA_ARGS=/c\KUBELET_EXTRA_ARGS=--node-ip=$IPADDR" /etc/sysconfig/kubelet
+# kubeletを再起動
+systemctl daemon-reload
+systemctl restart kubelet
+
+sudo swapoff -a
+sed -i '/ swap / s/^\\(.*\\)$/#\\1/g' /etc/fstab
